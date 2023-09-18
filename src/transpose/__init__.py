afe26113325e96563c4331dc3b377446383b628c
Ryan Reed <git@ryanreed.net>
2023-09-18 13:34:04 -0400

    Removing unused logger

--- src/transpose/__init__.py
+++ src/transpose/__init__.py
@@ -2,14 +2,10 @@ import os
 
 from importlib.metadata import version
 
-from .logger import create_logger
-
 DEFAULT_XDG_PATH = os.environ.get("XDG_DATA_HOME", f"{os.environ['HOME']}/.local/share")
 STORE_PATH = f"{DEFAULT_XDG_PATH}/transpose"
 DEFAULT_STORE_PATH = os.environ.get("TRANSPOSE_STORE_PATH", STORE_PATH)
 
 version = version("transpose")
-
-logger = create_logger(__package__)
 
 from .transpose import Transpose, TransposeConfig, TransposeEntry  # noqa: E402
